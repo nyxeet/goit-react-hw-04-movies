90294ce3bf1a58daa0a7aae8331ff0f173634254
nyxeet <nyxeet@ukr.net>
2021-02-10 21:33:15 +0200

    Update MoviesPage.js

--- src/views/MoviesPage.js
+++ src/views/MoviesPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import api from '../api/tv-api'
 import getQueryParams from '../utils/getQueryParams';
 import Searchbox from '../components/Searchbox'
+import { Link } from 'react-router-dom';
 
 
 class MoviesPage extends Component {
@@ -13,9 +14,19 @@ class MoviesPage extends Component {
         const { query } = getQueryParams(this.props.location.search);
         if (query) {
             api
-                .fetchShowWithQuery(query).then(shows => this.setState({shows}))
+                .fetchShowWithQuery(query).then(shows => this.setState({ shows: shows.results }))
         }
     }
+    componentDidUpdate(prevProps, prevState) {
+        const { query: prevQuery } = getQueryParams(prevProps.location.search);
+        const { query: nextQuery } = getQueryParams(this.props.location.search);
+
+        if (prevQuery !== nextQuery) {
+            api
+                .fetchShowWithQuery(nextQuery).then(shows => this.setState({ shows: shows.results }))
+        }
+    }
+    
     
 
     handleChangeQuery = query => {
@@ -25,12 +36,26 @@ class MoviesPage extends Component {
         });
     }
     render() {
+        const { shows } = this.state;
         return (
             <div>
                 <Searchbox onSubmit={this.handleChangeQuery} />
+                <ul>
+                    {shows && shows.map(show => (
+                        <li key={show.id}>
+                            <Link to = {{
+                            pathname: `/movies/${show.id}`, state: { from: this.props.location }, 
+                            }}>
+                                {show.original_title}
+                            </Link>
+                            
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
+
 }
 
 export default MoviesPage;
